c4d717370ca906809b8e5028768eeebe2f8671d5
anirudhmb <ambelathur@hotmail.com>
2019-10-03 14:58:42 +0530

    testing..

--- newClient.js
+++ newClient.js
@@ -5,8 +5,8 @@ ioClient.on("newMessage", (msg) => console.log("new message"+msg));
 
 ioClient.emit('privatechatroom', {email:"abcd"});
 
-ioClient.on('res', (msg) => console.log("res "+msg.msg));
+ioClient.on('res', (msg) => console.log("res"+msg.msg));
 
-ioClient.emit('new_private_message',{email:"abcd", clip_content:"new friend"});
+ioClient.emit('new_private_message',{email:"abcd", clip_content:"testing socket client"});
 
 ioClient.on('updated_clip_content', (msg) => console.log("chat room msg "+msg.clip_content));
